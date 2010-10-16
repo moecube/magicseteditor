moecube/magicseteditor

commit 0b1056664dacfaad49fb67857b06977818e094be

"scale down to" works for single line text boxes, fixes #53
git-svn-id: https://svn.code.sf.net/p/magicseteditor/code/trunk@1465 0fc631ac-6414-0410-93d0-97cfa31319b6

src/render/text/viewer.cpp
@@ -554,10 +554,13 @@ bool TextViewer::prepareLinesScale(RotatedDC& dc, const vector<CharInfo>& chars,
 		positions_word.push_back(word_size.width);
 		if (!c.soft) word_end_or_soft = i + 1;
 		// Did the word become too long?
-		if (style.field().multi_line && !break_now) {
+		if (!break_now) {
 			double max_width = lineRight(dc, style, line.top);
 			if (line_size.width + word_size.width > max_width) {
-				if (word_start == line.start) {
+				if (!style.field().multi_line) {
+					// single line word does not fit
+					return false;
+				} else if (word_start == line.start) {
 					// single word on this line; the word is too long
 					if (stop_if_too_long) {
 						return false; // just give up

commit 131e05b804a26fc033562bd35386bdf2be25e2a8

Fix error introduced in r1465: in single line text box, no lines were generated (which led to a crash), or text was scaled down when this was not intended.

src/render/text/viewer.cpp
@@ -350,6 +350,7 @@ void TextViewer::prepareElements(const String& text, const TextStyle& style, Con
 void TextViewer::prepareLines(RotatedDC& dc, const String& text, TextStyle& style, Context& ctx) {
 	vector<CharInfo> chars;
 	prepareLinesTryScales(dc, text, style, chars);
+	assert(!lines.empty());
 	
 	// store information about the content/layout, allow this to change alignment
 	style.content_width  = 0;
@@ -559,7 +560,7 @@ bool TextViewer::prepareLinesScale(RotatedDC& dc, const vector<CharInfo>& chars,
 			if (line_size.width + word_size.width > max_width) {
 				if (!style.field().multi_line) {
 					// single line word does not fit
-					return false;
+					if (stop_if_too_long) return false;
 				} else if (word_start == line.start) {
 					// single word on this line; the word is too long
 					if (stop_if_too_long) {
@@ -683,6 +684,7 @@ void TextViewer::alignLines(RotatedDC& dc, const vector<CharInfo>& chars, const 
 					-RealSize(style.padding_left+style.padding_right, style.padding_top + style.padding_bottom));
 	if (style.paragraph_height <= 0) {
 		// whole text box alignment
+		assert(!lines.empty());
 		double top = lines[0].top;
 		alignParagraph(0, lines.size(), chars, style, RealRect(RealPoint(0,top),s));
 	} else {